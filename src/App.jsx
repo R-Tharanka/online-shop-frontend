f2f09bea92aa4cd02b764706957a3b6568f18a1d
JinaNavo <it22070012@my.sliit.lk>
2026-03-07 02:09:38 +0530

    Created product service admin CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ProductServiceAdmin from './pages/ProductServiceAdmin'
 
 function App() {
   
 
   return (
 
-   
-    
-    <div className="bg-red-500 w-[100px]">Veloura</div>
-
-    
-    
+   <ProductServiceAdmin />
    
   )
 }
